4b2c690f061d83a8dc9af6a978a0e003da4d2bdd
Huu Dung Tran <huudung096@gmail.com>
2020-03-07 23:08:44 +0700

    Apply Hook

--- src/modules/todo/components/Input.js
+++ src/modules/todo/components/Input.js
@@ -1,46 +1,38 @@
-import React from "react";
-import { connect } from "react-redux";
-import * as todoAction from "../actions";
+import React, { useState, useEffect } from "react";
+import { connect } from 'react-redux';
+import * as todoAction from '../actions';
 
-class Input extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { value: "" };
-  }
+function Input(props) {
+  const [value, setValue] = useState('');
 
-  submitTodo() {
-    if (!this.state.value) {
+  const submitTodo = () => {
+    if (!value) {
       return;
-    } else {
-      this.props.dispatch(todoAction.add(this.state.value.trim()));
-    }
-    this.setState({ value: "" });
-    this.input.focus();
-  }
+    } 
+    props.dispatch(todoAction.add(value.trim()));
+    setValue('');
+  };
 
-  render() {
-    return (
-      <div class="form-input">
-        <form
-          onSubmit={e => {
-            e.preventDefault();
-            this.submitTodo();
-          }}
-        >
-          <div className="form-group">
-            <input
-              type="text"
-              className="form-control"
-              placeholder="type here"
-              onChange={e => this.setState({ value: e.target.value })}
-              value={this.state.value}
-              ref={node => (this.input = node)}
-            />
-          </div>
-        </form>
-      </div>
-    );
-  }
+  return (
+    <div class="form-input">
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          submitTodo();
+        }}
+      >
+        <div className="form-group">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="type here"
+            onChange={e => setValue(e.target.value)}
+            value={value}
+          />
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default connect()(Input);
